0750b0f7f1bc932e73d1e7ce602245f2b19634e2
elikimz <elijahkimani1293@gmail.com>
2025-01-06 19:06:34 +0300

    commit

--- src/fetchdata.tsx
+++ src/fetchdata.tsx
@@ -20,7 +20,7 @@ const Fetchdata = () => {
     
   return (
     <>
-    <section className="p-4 bg-gray-200 rounded-lg shadow-md">
+    <section className="p-4 bg-gray-300 rounded-lg shadow-md">
   <h2 className="text-2xl font-bold mb-4 text-center">Github Users</h2>
   <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
     {users.map((user) => {
